4bd0bbb8b89faa036fb183f6d95972532c7d0b75
modip <mordip06@gmail.com>
2021-09-14 07:55:35 +0000

    Some change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
                         <li><a class="dropdown-item" href="#!">Settings</a></li>
                         <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                         <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                        <li><a class="dropdown-item" href="/login">Deconnexion</a></li>
                     </ul>
                 </li>
             </ul>
@@ -98,7 +98,7 @@
                             </a>
                             <a class="nav-link" href="tables.html">
                                 <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tables
+                                Personnes
                             </a>
                         </div>
                     </div>
@@ -111,7 +111,7 @@
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid px-4">
-                        <h1 class="mt-4">Tables</h1>
+                        <h1 class="mt-4">Personnes</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
                             <li class="breadcrumb-item active">Tables</li>
